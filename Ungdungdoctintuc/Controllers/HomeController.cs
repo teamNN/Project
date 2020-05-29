7ba208df560dabb1355f5ec89f035f9101f77c8b
‘ivermin1123’ <mrkjin99@gmail.com>
2020-05-30 02:37:52 +0700

    update index

--- Ungdungdoctintuc/Controllers/HomeController.cs
+++ Ungdungdoctintuc/Controllers/HomeController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ungdungdoctintuc.Models;
 
 namespace Ungdungdoctintuc.Controllers
 {
+
+    
     public class HomeController : Controller
     {
+
+        DataClasses1DataContext data = new DataClasses1DataContext();
         public ActionResult Index()
         {
-            return View();
+            var tinmoi = LayTinNew(1);
+
+            return View(tinmoi);
+        }
+
+        private List<Tin> LayTinNew(int count)
+        {
+            // sắp xếp dữ liệu tin giảm dần theo ngày đăng ahihi
+            return data.Tins.OrderByDescending(a => a.NgayDang).Take(count).ToList();
         }
 
         public ActionResult About()
@@ -26,5 +39,27 @@ namespace Ungdungdoctintuc.Controllers
 
             return View();
         }
+
+        public ActionResult NavHeader()
+        {
+            var All_Theloai = from tt in data.TheLoais select tt;
+            return PartialView(All_Theloai);
+        }
+
+        public ActionResult ChuyenMuc(int id)
+        {
+            var chuyenmuc = data.ChuyenMucs.Where(m => m.IdTheLoai==id).ToList();
+            return View(chuyenmuc);
+        }
+
+        public ActionResult LayUrl(int id)
+        {
+            var urlHinh = data.Urls.Where(m => m.IdTin == id).SingleOrDefault();
+            return PartialView(urlHinh);
+        }
+          
+
+    
+
     }
 }
